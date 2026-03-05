f699ca5bb2dcf24d7b6df6f710b0609ab73b27c5
Cassianus <masterticole@gmail.com>
2026-03-05 19:38:46 -0300

    w speed

--- tabuada.py
+++ tabuada.py
@@ -1,10 +1,20 @@
+import time
+import webbrowser
 valor = int(input("Valor da tabuada: "))
-tabuada = 0
-if valor >= 0:
+tabuada = 1
+if valor > 0:
+    print(f"Tabuada do {valor}")
     while tabuada <= 10:
         resultado = valor * tabuada
         print(f"o resultado é {resultado}")
         tabuada += 1
+        time.sleep(1)
     print("W tabuada")
+    time.sleep(1)
+    webbrowser.open_new_tab("https://media.tenor.com/q4RChyNtpC8AAAAe/w-speed-ishowspeed.png")
 else:
-    print("Valor positivo")
+    print("Valor positivo porra")
+    time.sleep(1)
+    print("L total pra vc")
+    time.sleep(1)
+    webbrowser.open_new_tab("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS0dehWfIbOwJGDavWz-cpPXRzx5kg8H9l_Sw&s")
